fd57c673c7668ad18cd4610a412789995701dec5
kyohei.shibuya <shibuya01055@gmail.com>
2023-06-04 01:39:17 +0900

    test active hash

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_03_161754) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.integer "prefecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
